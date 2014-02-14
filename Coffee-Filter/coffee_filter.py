0d90d9618c52fab6e5683db61845f9c7547f3cbd
MrMcKizzle <ziesunkownemail@clueless.duh>
2014-02-13 21:14:11 -0700

    using a guassian blend to smooth the data.

--- Coffee-Filter/coffee_filter.py
+++ Coffee-Filter/coffee_filter.py
@@ -15,9 +15,6 @@ def get_data():
                        .3505,.3400,.3297,.3181,.3051,.2913,.2788,.2667,.2497,.2337,
                        .2175,.2008,.1846,.1696,.1566,.1393,.1263]])
 
-""" 
-
-"""
 def main():
     data = get_data()
 
@@ -29,20 +26,100 @@ def main():
             [data[0, index] for index in range(1, data.shape[1] - 1)],
             [fd.acceleration(index, data) for index in range(1, data.shape[1] - 1)]
         ])
-
-    plt.subplot(221)
-    plt.plot(data[0,:], data[1,:], "b")
-    plt.subplot(222)
-    plt.plot(data_veloc[0,:], data_veloc[1,:], "g")
-    plt.subplot(223)
-    plt.plot(data_accel[0,:], data_accel[1,:], "g")
-    plt.show()
-
-    ax = ax3d(plt.gcf())
-    ax.plot(data_veloc[0,:], data_accel[1,:], data_veloc[1,:])
-    plt.show()
-
     
+
+    #plt.subplot(221)
+    #plt.plot(data[0,:], data[1,:], "ro-")
+    #plt.ylabel("Position (m)")
+    #plt.xlabel("Time (sec)")
+    #plt.title("Position vs Time")
+    #plt.subplot(222)
+    #plt.plot(data_veloc[0,:], data_veloc[1,:], "go-")
+    #plt.ylabel(r"Velocity ($\frac{m}{s}$)")
+    #plt.xlabel("Time (sec)")
+    #plt.title("Velocity vs Time")
+    #plt.subplot(223)
+    #plt.plot(data_accel[0,:], data_accel[1,:], "ko-")
+    #plt.ylabel(r"Acceleration ($\frac{m}{s^2}$)")
+    #plt.xlabel("Time (sec)")
+    #plt.title("Acceleration vs Time")
+    #plt.subplot(224)
+    #plt.plot(data_accel[1,:], data_veloc[1,:], "ko-")
+    #plt.ylabel(r"Velocity ($\frac{m}{s}$)")
+    #plt.ylabel(r"Acceleration ($\frac{m}{s^2}$)")
+    #plt.title("Acceleration vs Velocity")
+    #plt.show()
+    
+    # Calculate the window wieghts for the guassian blend. 
+    wwghts = np.array([gauss_weights(r, 1) for r in [2, 1, 0, 1, 2]])
+    wwghts = wwghts / np.sum(wwghts)
+    bnds = int(len(wwghts)/2)
+    
+    # Apply guassian blend to the acceleration and velocity data. 
+    data_vgss = np.array([
+        [data_veloc[0, index] for index in range(bnds, data_veloc.shape[1] - bnds)],
+        [gaussian_blend(index, data_veloc[1,:], wwghts) for index in range(bnds, data_veloc.shape[1] - bnds)]
+    ])
+
+    data_agss = np.array([
+        [data_accel[0, index] for index in range(bnds, data_accel.shape[1] - bnds)],
+        [gaussian_blend(index, data_accel[1,:], wwghts) for index in range(bnds, data_accel.shape[1] - bnds)]
+    ])
+
+    # Determine the time the acceleration first reaches zero. 
+    tv_time = 0.0 # the point the object stops accelerating (terminal velocity)
+    tv_index = 0.0  
+    for i in range(0, data_agss.shape[1]):
+        if data_agss[1, i] >= 0 and i > 0:
+            tv_time = data_agss[0,i]
+            break
+        elif data_agss[1, i] >= 0:
+            tv_time = data_agss[0,i]
+            break
+
+    print zerodex
+    
+    #plt.subplot(221) 
+    #plt.plot(data_veloc[0,:], data_veloc[1,:], "go-")
+    #plt.plot(data_vgss[0,:], data_vgss[1,:], "ro-")
+    #plt.ylabel(r"Velocity ($\frac{m}{s}$)")
+    #plt.xlabel("Time (sec)")
+    #plt.title("Velocity vs Time")
+    #plt.subplot(222) 
+    #plt.plot(data_accel[0,:], data_accel[1,:], "go-")
+    #plt.plot(data_agss[0,:], data_agss[1,:], "ro-")
+    #plt.ylabel(r"Acceleration ($\frac{m}{s^2}$)")
+    #plt.xlabel("Time (sec)")
+    #plt.title("Acceleration vs Time")
+    #plt.subplot(224)
+    #plt.plot(data_vgss[1,:], data_agss[1,:], "ko-")
+    #plt.xlabel(r"Velocity ($\frac{m}{s}$)")
+    #plt.ylabel(r"Acceleration ($\frac{m}{s^2}$)")
+    #plt.title("Acceleration vs Velocity")
+    #plt.show()
+    #plt.show()
+
+
+    #gaussian_blend(2, data[1,:], window_weights)
+
+    #ax = ax3d(plt.gcf())
+    #ax.plot(data_veloc[0,:], data_accel[1,:], data_veloc[1,:])
+    #plt.show()
+
+# Used to calculate the gauss weights for the sliding window. 
+def gauss_weights(r, sigma):
+    return np.exp(-(r**2) / (2 * sigma**2))
+
+# Assumes that the data is an numpy array.
+def gaussian_blend(index, data, window):
+    deg = int(len(window)/2)
+    start = index - deg
+    stop = index + deg 
+    subset = [data[i] for i in range(start, stop)]
+    gaussian_sum = np.sum([subset[i] * window[i] for i in range(0, len(subset))])
+
+    return gaussian_sum
+     
 
 
 if __name__ == "__main__":
