f575c17479634d94226ccc2578a3afd6281f03a1
Clinton McKay <ziesunkownemail@clueless.duh>
2014-02-16 20:23:28 -0700

    plt2dcmpr now allows the line styles to be passed in as arguments

--- Coffee-Filter/utils.py
+++ Coffee-Filter/utils.py
@@ -3,9 +3,9 @@ import numpy as np
 
 # Plot two systems together for comparing.
 # http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.plot
-def plt2dcmpr(X1, Y1, X2, Y2, legend_labels, xlab, ylab, title):
-    plt1, = plt.plot(X1, Y1, "b")
-    plt2, = plt.plot(X2, Y2, "r")
+def plt2dcmpr(X1, Y1, X2, Y2, styles, legend_labels, xlab, ylab, title):
+    plt1, = plt.plot(X1, Y1, styles[0])
+    plt2, = plt.plot(X2, Y2, styles[1])
     plt.legend([plt1, plt2], legend_labels)
     plt.ylabel(ylab)
     plt.xlabel(xlab)
