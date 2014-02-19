8ca868fac21c52f36c5b3e599992fa4b3cfc1894
Clinton McKay <ziesunkownemail@clueless.duh>
2014-02-18 17:26:29 -0700

    Problem One is almost complete. Now simply produce a systematic method of exploring ways to reduce the sum squares error

--- homework1/problem1.py
+++ homework1/problem1.py
@@ -3,20 +3,31 @@ import numpy as np
 import ode as ode
 import system as sys
 import math as math
+import utils as ut
 
-def run():
-    sim_e = sys.simulate([np.array([0])], 0.1, 0, 1.5, erf_ode, ode.euler)
-    sim_er = sys.simulate([np.array([0])], 0.1, 0, 1.5, erf_ode, ode.euler_richardson)
-    sim_rk4 = sys.simulate([np.array([0])], 0.1, 0, 1.5, erf_ode, ode.runge_kutta) 
+def run(): 
+    dt = 0.1
+    t_0 = 0.0
+    t_max = 1.5
+    odes = [ode.euler, ode.euler_richardson, ode.runge_kutta] 
+    ode_ldgns = ["Euler", "Euler Richardson", "Runge Kutta"]
+    plt_files = ["erf_euler", "erf_euler-richardson", "erf_rk4"]
+
     anl_data = sys.simulate([np.array([0])], 0.1, 0, 1.5, erf, ode.analytical)
-
-    # TODO Compute the sum squared error of your erf and pythons erf.
-
-    plt.plot(sim_e[:, 0], sim_e[:, 1])
-    plt.plot(sim_er[:, 0], sim_er[:, 1])
-    plt.plot(sim_rk4[:, 0], sim_rk4[:, 1])
-    plt.plot(anl_data[:, 0], anl_data[:, 1])
-    plt.show()
+    for i in range(0, len(odes)):
+        sim_d = sys.simulate([np.array([0])], 0.1, 0, 1.5, erf_ode, odes[i])
+        
+        # compute the sum squared error
+        sum_sqrs = np.sum((sim_d[:, 1] - anl_data[:, 1])**2)
+        
+        ut.plt2dcmpr(
+                sim_d[:,0], sim_d[:,1], anl_data[:,0], anl_data[:,1],
+                ["bs-", "ro:"], [ode_ldgns[i], "Python erf(x)"],
+                "t", "x", "Pyrthon erf(x) vs ODE erf(x) %s Integration" % ode_ldgns[i]
+                )
+        plt.text(0.8, 0.4, "Sum Squares Error = %0.4f" % sum_sqrs)
+        plt.savefig("%s.png" % plt_files[i], bbox_inches='tight')
+        plt.close()
 
 def erf_ode(x, t):
     """ ODE system for erf(t) """
