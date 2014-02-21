10682ce80a1a17aa4386a6ddb425c158603a7a54
Clinton McKay <ziesunkownemail@clueless.duh>
2014-02-20 18:09:07 -0700

    jupiter seems to be orbiting correctly. Now lets debug Earth!

--- Python-ODE-Int-NBody/main.py
+++ Python-ODE-Int-NBody/main.py
@@ -22,17 +22,19 @@ M_JUP= 0.04 * GM
 
 def main():
     # Earth initial Cond
-    x_1 = 2.52
-    y_1 = 0.0
-    v_x1 = 0.0
-    v_y1 = mt.sqrt(GM / x_1)
+    x_e = 2.52
+    y_e = 0.0
+    v_xe = 0.0
+    v_ye = mt.sqrt(GM / x_e)
 
     # Jupiter Initial Conditions
-    x_2 = 5.24
-    y_2 = 0.0
-    v_x2 = 0.0
-    v_y2 = mt.sqrt(GM / x_2) 
-    init_cond = np.array([x_1, y_1, v_x1, v_x1, x_2, y_2, v_x2, v_y2]) 
+    x_j = 5.24
+    y_j = 0.0
+    v_xj = 0.0
+    v_yj = mt.sqrt(GM / x_j) 
+
+
+    init_cond = np.array([x_e, y_e, v_xe, v_ye, x_j, y_j, v_xj, v_yj]) 
     
     # System Settings
     dt = 0.01
@@ -50,28 +52,24 @@ def main():
     plt.show()
 
 
-
 def planets(x, t, GM=GM, m_e=M_EAR, m_j=M_JUP):
     """ Planet ODE system """
     dxdt = np.zeros(np.size(x))
-    
-    r_se = np.sqrt(np.sum(x[XE:(YE + 1)]**2))                      # r_1 between sun and earth.
-    r_ej = np.sqrt(np.sum((x[XJ:(YJ + 1)] - x[XE:(YE + 1)])**2))   # r_21 between earth and jupiter.
-    r_sj = np.sqrt(np.sum(x[XJ:(YJ + 1)]**2))                      # r_2 between sun and jupiter.
-
-    #print r_se, r_ej, r_sj
-    #exit()
-    
+     
+    r_se = np.sqrt(np.sum(x[XE:(YE + 1)]**2))                           # r_se between sun and earth.
+    r_ej = np.sqrt(np.sum(x[XJ:(YJ + 1)]**2 + x[XE:(YE + 1)]**2))       # r_ej between earth and jupiter.
+    r_sj = np.sqrt(np.sum(x[XJ:(YJ + 1)]**2))                           # r_sj between sun and jupiter.
+     
     # Earth
     dxdt[XE]    = x[VXE]
     dxdt[YE]    = x[VYE]
-    dxdt[VXE]   = (GM / r_se**3 * x[XE]) + (GM * m_e / r_ej**3 * (x[XJ] - x[XE]))
-    dxdt[VYE]   = (-1 * GM / r_se**3 * x[YE]) + (GM * m_j / r_ej**3 * (x[YJ] - x[YE]))
+    dxdt[VXE]   = -GM / r_se**3 * x[XE] + GM * m_j / r_ej**3 * (x[XJ] - x[XE])
+    dxdt[VYE]   = -GM / r_se**3 * x[YE] + GM * m_j / r_ej**3 * (x[YJ] - x[YE]) 
     # Jupiter
     dxdt[XJ]    = x[VXJ]
     dxdt[YJ]    = x[VYJ]
-    dxdt[VXJ]   = (GM / r_sj**3 * x[XJ]) + (GM * m_j / r_ej**3 * (x[XE] - x[XJ]))
-    dxdt[VYJ]   = (-1 * GM / r_sj**3 * x[YJ]) + (GM * m_j / r_ej**3 * (x[YE] - x[YJ]))
+    dxdt[VXJ]   = -GM / r_sj**3 * x[XJ] - GM * m_e / r_ej**3 * (x[XE] - x[XJ])
+    dxdt[VYJ]   = -GM / r_sj**3 * x[YJ] - GM * m_e / r_ej**3 * (x[YE] - x[YJ])
 
     #print dxdt
 
