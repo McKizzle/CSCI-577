0cbfc83b23764b0274be10d0ee5c6bc82eb2509a
Clinton McKay <ziesunkownemail@clueless.duh>
2014-02-21 08:51:45 -0700

    Woops! No wonder the planets werent orbiting properly. I gave them the incorrect masses.

--- Python-ODE-Int-NBody/main.py
+++ Python-ODE-Int-NBody/main.py
@@ -17,8 +17,8 @@ VYJ= 7
 
 # Constants
 GM = 4 * mt.pi ** 2
-M_EAR = 0.001 * GM
-M_JUP= 0.04 * GM
+M_EAR = 0.001
+M_JUP= 0.04
 
 def main():
     # Earth initial Cond
@@ -41,7 +41,7 @@ def main():
     t_0 = 0.0
     t_max = 100
     ode_sys = planets
-    integrator = ode.runge_kutta
+    integrator = ode.euler_richardson
 
     print "Simulating NBody..."
     sim_d = sys.simulate([init_cond], dt, t_0, t_max, ode_sys, integrator)
@@ -55,10 +55,16 @@ def main():
 def planets(x, t, GM=GM, m_e=M_EAR, m_j=M_JUP):
     """ Planet ODE system """
     dxdt = np.zeros(np.size(x))
+
+    #print "x:\t", x
      
     r_se = np.sqrt(np.sum(x[XE:(YE + 1)]**2))                           # r_se between sun and earth.
-    r_ej = np.sqrt(np.sum(x[XJ:(YJ + 1)]**2 + x[XE:(YE + 1)]**2))       # r_ej between earth and jupiter.
+    r_ej = np.sqrt(np.sum((x[XJ:(YJ + 1)] - x[XE:(YE + 1)])**2))        # r_ej between earth and jupiter.
     r_sj = np.sqrt(np.sum(x[XJ:(YJ + 1)]**2))                           # r_sj between sun and jupiter.
+
+    #print "r_se:\t%0.4f"% r_se
+    #print "r_ej:\t%0.4f"% r_ej
+    #print "r_sj:\t%0.4f"% r_sj
      
     # Earth
     dxdt[XE]    = x[VXE]
@@ -71,7 +77,9 @@ def planets(x, t, GM=GM, m_e=M_EAR, m_j=M_JUP):
     dxdt[VXJ]   = -GM / r_sj**3 * x[XJ] - GM * m_e / r_ej**3 * (x[XE] - x[XJ])
     dxdt[VYJ]   = -GM / r_sj**3 * x[YJ] - GM * m_e / r_ej**3 * (x[YE] - x[YJ])
 
-    #print dxdt
+    #print "DxDt:\t", dxdt
+
+    #exit()
 
     return dxdt
 
