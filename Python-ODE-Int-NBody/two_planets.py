d2d0ac39a95295ae9556818358c9d2a1934b75e4
Clinton McKay <ziesunkownemail@clueless.duh>
2014-02-23 11:34:01 -0700

    % delta functions recalculated properly now. Also plot lables do not overlap

--- Python-ODE-Int-NBody/two_planets.py
+++ Python-ODE-Int-NBody/two_planets.py
@@ -10,16 +10,15 @@ import matplotlib.pyplot as plt
 # Constants
 G = 6.67384e-11
 GM = 4 * mt.pi ** 2
-#M = 1.989e30
 M_EAR = 0.001
 M_JUP= 0.04
 
 def main(): 
-    dt = 0.01
+    dt = 0.001
     t_0 = 0.0
     t_max = 100
 
-    # Simulate the two planets. 
+    # Simulate the two planets using the scipy integrator
     init_cond = two_planets_initial_cond()
     intgr = scint.ode(two_planets)
     intgr.set_integrator('vode', atol=0.00001, rtol=0.00001)
@@ -32,36 +31,42 @@ def main():
     sim_d = np.array(sim_d)
     
     # Plot the orbits
+    plt.subplots(nrows=3, ncols=1)
+    plt.tight_layout()
     plt.subplot(311)
     plt.axis("equal")
     ut.plt2dcmpr(sim_d[:, 0], sim_d[:, 1], sim_d[:, 4], sim_d[:, 5],
-        ["r", "b"], ["Earth", "Jupiter"], "X (AU)", "Y (AU)", "Jupiter and Earth Orbit Simulation Results")
+        ["r", "b"], ["Earth", "Jupiter"], "X (AU)", "Y (AU)", "Jupiter and Earth Orbit Simulation Results (dt = %0.3f)" % dt)
+
+    # Now simulate the system using the class built integrator.
+    sim_c = sys.simulate([two_planets_initial_cond()], dt, t_0, t_max, two_planets_wrapper, ode.runge_kutta)
+    #ut.plt2dcmpr(sim_c[:, 0], sim_c[:, 1], sim_c[:, 4], sim_c[:, 5],
+    #    ["r", "b"], ["Earth", "Jupiter"], "X (AU)", "Y (AU)", "Jupiter and Earth Orbit Simulation Results")
 
     # Calcualte the percent delta change for the energy and angular momentum.
     sim_perde = []
     sim_perdam = []
-    for i in range(1, sim_d.shape[0]):
-        sim_perde.append(rel_diff(two_planets_energy(sim_d[i - 1]), two_planets_energy(sim_d[i])))
-        sim_perdam.append(rel_diff(two_planets_angular_momentum(sim_d[i - 1]),
-            two_planets_angular_momentum(sim_d[i])))
+    for i in range(0, sim_d.shape[0]):
+        sim_perde.append(per_diff(two_planets_energy(sim_d[i]), two_planets_energy(sim_c[i])))
+        sim_perdam.append(per_diff(two_planets_angular_momentum(sim_d[i]), two_planets_angular_momentum(sim_c[i])))
     
     # Plot 
     sim_perde = np.array(sim_perde)
     sim_pardam = np.array(sim_perdam)
     plt.subplot(312)
-    plt.plot(times[1:times.shape[0]], sim_perde)
+    plt.plot(times, sim_perde)
     plt.xlabel("time (years)")
     plt.ylabel(r"% $\Delta$ E / M")
     plt.subplot(313)
-    plt.plot(times[1:times.shape[0]], sim_perdam)
+    plt.plot(times, sim_perdam)
     plt.xlabel("time (years)")
     plt.ylabel(r"% $\Delta$ L / M")
-    plt.savefig("two_planets.png", bbox_inches="tight")
+    plt.savefig("two_planets.png", bbox_inches="tight", dpi=120)
     plt.close()
 
 
-def rel_diff(x_1, x_2):
-    return (x_2 - x_1) / x_1
+def per_diff(x_1, x_2):
+    return (x_2 - x_1) / x_1 * 100
 
 def two_planets_energy(x, GM=GM, m_1overM=M_EAR, m_2overM=M_JUP):
     # Build the vectors. 
@@ -72,17 +77,23 @@ def two_planets_energy(x, GM=GM, m_1overM=M_EAR, m_2overM=M_JUP):
     r_1     = np.linalg.norm(vr_1) 
     r_21    = np.linalg.norm(vr_21)
     r_2     = np.linalg.norm(vr_2)
+    # Construct the Velocity magnitudes.
+    v_1 = np.linalg.norm(x[2:4])
+    v_2 = np.linalg.norm(x[6:8])
 
-    EoverM = 1.0/2.0*m_1overM*vr_1**2 + 1.0/2.0*m_2overM*vr_2**2 \
+    EoverM = 1.0/2.0*m_1overM*v_1**2 + 1.0/2.0*m_2overM*v_2**2 \
             - GM * m_1overM / r_1 - GM * m_2overM / r_2 \
             - GM * m_1overM * m_2overM / r_21
-    return np.linalg.norm(EoverM)
+    return EoverM
 
 def two_planets_angular_momentum(x, GM=GM, m_1overM=M_EAR, m_2overM=M_JUP):
     # Build the vectors. 
     L_1overM = m_1overM*(x[0]*x[3] - x[1]*x[2])
     L_2overM = m_1overM*(x[4]*x[7] - x[5]*x[6])
     return L_1overM + L_2overM
+
+def two_planets_wrapper(x, t):
+    return two_planets(t, x)
  
  
 def two_planets(t, x, GM=GM, m_1overM=M_EAR, m_2overM=M_JUP):
