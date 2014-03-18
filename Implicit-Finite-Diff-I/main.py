8145c22a858505bd01ec12b8a0d2826879a87510
MrMcKizzle <ziesunkownemail@clueless.duh>
2014-03-18 10:05:53 -0600

    added empty script file for IFDI lab

---
+++ Implicit-Finite-Diff-I/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import scipy.signal as sig
+import numpy as np
+import math as mt
+import matplotlib.pyplot as plt
+import time as time
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
