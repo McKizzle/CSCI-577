c056281d6cee2dd6f7ae31dd53dd9b000b14cd27
Clinton McKay <ziesunkownemail@clueless.duh>
2014-03-23 11:23:04 -0600

    added the Stability-And-Storage code. nothing implemented only math

---
+++ Stability-And-Storage/main.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+import scipy.signal as sig
+import scipy as scpy
+import numpy as np
+import math as mt
+import matplotlib.pyplot as plt
+import time as tm
+
+def main():
+    N = 51
+    time_steps = 2000
+    a = 0
+    b = 1
+    x_0 = 0.0
+    x_N = 1e3
+    X = np.linspace(x_0, x_N, N)
+    dx = X[1] - X[0]
+    dt = 1
+    u = np.zeros(N)
+    u[int(N/2)] = b 
+    alpha = dt / dx**2.0
+
+    I = scpy.identity(N) 
+    
+    # Build the A matrix
+    A = scpy.sparse.lil_matrix((N, N))
+    A.setdiag(np.zeros(N) - 2.0 * alpha)
+    A.setdiag(np.zeros(N) + alpha, k=1)
+    A.setdiag(np.zeros(N) + alpha, k=-1)
+
+    S = (I - A)
+    S = scpy.sparse.csr_matrix(S)
+    
+    U = simulate(u, S, n=time_steps)
+    
+    return 0
+
+def integral(x, u):
+    """ Calculate the area underneath a curve.
+        :param x: the x values of the curve. 
+        :param u: the y values of the curve.
+    """
+    area = 0
+    h = x[1] - x[0]
+    for i in range(1, len(u)):
+        p0 = np.array([x[i - 1], 0])
+        p1 = np.array([x[i - 1], u[i - 1]])
+        p2 = np.array([x[i], u[i]])
+        p3 = np.array([x[i], 0])
+        a = np.linalg.norm(p0 - p1)
+        b = np.linalg.norm(p2 - p3)
+        area = area + 0.5 * (a + b) * h
+    
+    return area
+
+def simulate(u_0, A, boundries = 0.0, n=2000):
+    """ Run the simulation for a defined number of steps 
+        :param 
+        Returns the u^{n}
+    """
+    U = [] 
+    u_n = u_0
+    for i in range(0, n):
+        U.append(u_n)
+        u_np1 = scpy.sparse.linalg.spsolve(A,u_n) 
+        u_np1[0] = boundries
+        u_np1[-1] = boundries
+        u_n = u_np1
+        
+    return U
+
+def animate1Dframes(x, data):
+    """ Animates a 2D array of data using pyplot. 
+        :param x: the x values to plot over
+        :param y: the y value 'frames' to plot at each iteration. 
+
+        Follows example at http://www.lebsanft.org/?p=48
+    """
+    plt.ion() # Set the plot to animated.    
+    ax1 = plt.axes()
+    line, = plt.plot(x, data[0], '-ok')
+
+    for u in data:
+        line.set_ydata(u)
+        plt.draw()
+
+if __name__ == "__main__":
+    main()
